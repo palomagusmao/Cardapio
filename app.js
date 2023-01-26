52161ce8c035c567cd5bd348c744b73257e4f117
Paloma Oliveira Gusmão <palomagomes.oliver@gmail.com>
2023-01-26 00:39:59 -0300

    finalizando o layout

---
+++ app.js
@@ -0,0 +1,35 @@
+const pizza = [ 
+    {
+        id:1,
+        nome: "Pizza de Calabresa",
+        categoria: "doce",
+        preco: 45.00,
+        img:"./img/",
+        descricao: 'hhsuhshsasshsnashnasn',
+    },
+    {
+        id:2,
+        nome: "Pizza de Queijo",
+        categoria: "doce",
+        preco: 45.00,
+        img:"./img/",
+        descricao: 'hhsuhshsasshsnashnasn',
+    },
+    {
+        id:3,
+        nome: "Pizza de Chocolate",
+        categoria: "doce",
+        preco: 45.00,
+        img:"./img/",
+        descricao: 'hhsuhshsasshsnashnasn',
+    },
+    {
+        id:4,
+        nome: "Pizza de Banana",
+        categoria: "doce",
+        preco: 45.00,
+        img:"./img/",
+        descricao: 'hhsuhshsasshsnashnasn',
+    },
+
+]
